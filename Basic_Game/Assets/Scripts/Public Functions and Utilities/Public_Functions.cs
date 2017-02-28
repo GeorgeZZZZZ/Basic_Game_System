e29400d8c6e83f6abc7ef6b2c80e79ca8f6a6bcd
GeorgeZZZZZ <pknlm789@gmail.com>
2017-02-28 21:51:25 +0800

    Initiallize the project
    Project from
    Camera_and_Control_System
    ver. 43a5f8d2ba939f7cb4cd8dc7c14b4a0597b37fda

---
+++ Basic_Game/Assets/Scripts/Public Functions and Utilities/Public_Functions.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//	0.3.0
+public class Public_Functions : MonoBehaviour {
+
+	// Smooth Angle transform function
+	public static void SMO_ANG (float curAng, float tarAng, float smoTim, out float result) {
+		float smoothV = 0f;	//calculation temp cache for Mathf.SmoothDamp
+
+		result = Mathf.SmoothDampAngle (curAng, tarAng, ref smoothV, smoTim);
+	}
+
+	// Smooth Value transform function
+	public static void SMO_VAL (float curVal, float tarVal, float smoTim, out float result) {
+		float smoothV = 0f;	//calculation temp cache for Mathf.SmoothDamp
+
+		result = Mathf.SmoothDamp (curVal, tarVal, ref smoothV, smoTim);
+	}
+
+	//	Smooth Value and give Offset
+	//	cache: cache (current Value), target: target value, smoothDamp: smoothDamp
+	//	out cacheOut: cache value output, out offset: offset result after calculation
+	public static void SMO_VAL_OFFSET (float cache, float target, float smoothDamp, out float cacheOut, out float offset) {
+		float oldDisOff = cache;	//	save old movement calculation cache value
+
+		SMO_VAL (cache, target, smoothDamp, out cacheOut);	//	smooth movemeant
+		offset = (cacheOut - oldDisOff);	//	calculate offset value
+	}
+
+	public static float Clear_Angle (float angleIn) {
+		//	take away unnecessary number, limit angle in 360
+		float newAng = angleIn - (360f * Mathf.Floor (angleIn / 360f));	
+
+		if (newAng < -180f) {
+			return (360f + newAng);
+		} else if (newAng > 180f) {
+			return (-360f + newAng);
+		} else {
+			return newAng;
+		}
+	}
+
+	public static bool Mous_Click_Get_Pos_Dir (Camera cam,Transform curTrans,int maskIn,out Vector3 hitPos, out Quaternion tarRote) {
+		
+		Ray camRay = cam.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, 100f, maskIn)) {
+			hitPos = floorHit.point;
+			Vector3 playerToMouse = floorHit.point - curTrans.position;
+			playerToMouse.y = 0f;
+			tarRote = Quaternion.LookRotation (playerToMouse);
+
+			return true;
+		} else {
+			hitPos = Vector3.zero;
+			tarRote = Quaternion.Euler (Vector3.zero);	//	has to return 0
+			return false;
+		}
+	}
+}
